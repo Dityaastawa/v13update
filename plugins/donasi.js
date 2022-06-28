8ada9be34c626dfd4f71972763947def12aa7867
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-06-28 19:11:36 +0800

    Add files via upload

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [085792429140]
+│ •  [085792429140]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
